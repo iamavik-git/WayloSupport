dbedfd57317c8e72fe3ac68fb7822bd08bf57a90
Avik Majumder <whothehellareu5@gmail.com>
2018-06-13 12:11:36 +0530

    New Activity

---
+++ app/src/main/java/com/example/rahul/waylosupport/Main2Activity.java
@@ -0,0 +1,34 @@
+package com.example.rahul.waylosupport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.suke.widget.SwitchButton;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        com.suke.widget.SwitchButton switchButton = (com.suke.widget.SwitchButton)
+                findViewById(R.id.switch_button);
+
+        switchButton.setChecked(true);
+        switchButton.isChecked();
+        switchButton.toggle();     //switch state
+        switchButton.toggle(false);//switch without animation
+        switchButton.setShadowEffect(true);//disable shadow effect
+        switchButton.setEnabled(false);//disable button
+        switchButton.setEnableEffect(false);//disable the switch animation
+        switchButton.setOnCheckedChangeListener(new SwitchButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SwitchButton view, boolean isChecked) {
+                //TODO do your job
+            }
+        });
+
+    }
+}
