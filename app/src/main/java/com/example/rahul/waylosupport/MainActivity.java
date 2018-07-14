2ae478e5b0752e603c65396ff49acda9833c9390
Avik Majumder <whothehellareu5@gmail.com>
2018-07-14 09:27:48 +0530

    design

--- app/src/main/java/com/example/rahul/waylosupport/MainActivity.java
+++ app/src/main/java/com/example/rahul/waylosupport/MainActivity.java
@@ -29,7 +29,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
-//import com.suke.widget.SwitchButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,22 +48,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setCustomView(R.layout.ab_layout);
 
 
-//        com.suke.widget.SwitchButton switchButton = (com.suke.widget.SwitchButton)
-//                findViewById(R.id.switch_button);
-//
-//        switchButton.setChecked(true);
-//        switchButton.isChecked();
-//        switchButton.toggle();     //switch state
-//        switchButton.toggle(true);//switch without animation
-//        switchButton.setShadowEffect(true);//disable shadow effect
-//        switchButton.setEnabled(true);//disable button
-//        switchButton.setEnableEffect(true);//disable the switch animation
-//        switchButton.setOnCheckedChangeListener(new SwitchButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(SwitchButton view, boolean isChecked) {
-//                //TODO do your job
-//            }
-//        });
 
 
 
